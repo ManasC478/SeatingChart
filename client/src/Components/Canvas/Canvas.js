15839e9f035653cba40c2e468f112c98b2851410
Kefir101 <kefir.sade.bar@gmail.com>
2021-09-22 18:30:14 -0700

    fixed algorithm finding max layout

--- client/src/Components/Canvas/Canvas.js
+++ client/src/Components/Canvas/Canvas.js
@@ -5,11 +5,14 @@ import './style.css';
 
 const Canvas = () => {
     const [assignedSeats, setAssignedSeats] = useState([]);
+    const [seatingChartScore, setSeatingChartScore] = useState(null);
     
     const handleAssignSeats = async () => {
         try {
-            const { data: { studentList } } = await assignSeats(['dummy', 'list', 'sent', 'to', 'backend']);
-            setAssignedSeats(studentList);    
+            const { data: { studentList, bestSeatingChartScore } } = await assignSeats(['dummy', 'list', 'sent', 'to', 'backend']);
+            console.log(studentList);
+            setAssignedSeats(studentList);
+            setSeatingChartScore(bestSeatingChartScore)
         } catch (error) {
             console.log(error);
         }
@@ -17,12 +20,27 @@ const Canvas = () => {
 
     return (
         <main className="canvas">
+          { 
+            console.log(assignedSeats)
+          }
             <button onClick={handleAssignSeats}>Click to Assign Seats</button>
+            <h3>{seatingChartScore}</h3>
             <div className="student-grid">
                 {
                     assignedSeats.map((student, index) => {
+                      const {name, sitNextTo, doNotSitNextTo, happy, sad, frontPreference, backPreference} = student;
+                      const [sitNextToStudent1, sitNextToStudent2] = sitNextTo;
+                      const [dontSitNextToStudent1, dontSitNextToStudent2] = doNotSitNextTo;
                         return (
-                            <p key={index}>{student}</p>
+                          <div key={index} class="student-item">
+                            <p><strong>Name:</strong> {name}</p> <br></br>
+                            {/* <p><strong>sitNextTo:</strong> {sitNextToStudent1}, {sitNextToStudent2}</p>
+                            <p><strong>doNotSitNextTo:</strong> {dontSitNextToStudent1}, {dontSitNextToStudent2}</p> */}
+                            <p><strong>Happy With:</strong> {happy}</p>
+                            <p><strong>Sad With:</strong> {sad}</p>
+                            <p><strong>Front Pref:</strong> {frontPreference ? 'yes' : 'idc'}</p>
+                            <p><strong>Back Pref:</strong> {backPreference ? 'yes' : 'idc'}</p>
+                          </div>
                         );
                     })
                 }
--- server/Controllers/algorithm.js
+++ server/Controllers/algorithm.js
@@ -2,57 +2,63 @@
 // if you do:
 // make sure to change router.get('/algorithm', assignSeats) in routes/algorithm.js to router.get('/algorithm', newName)
 // and the name in the import in router/algorithm.js
-
+const gridHeight = 3;
+const gridWidth = 4;
+const studentCount = gridWidth * gridHeight;
+const preferenceCount = 2;
+let seatingChartScore = 0;
 module.exports.assignSeats = (req, res) => {
-  // a list I used for testing. you can delete it
-  // const dummyList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
   try {
+    let students = [];
     // put your code in the try
     // you can have regular js function outside of the module.exports as helper functions
     // if an error occurs the code will automatically go to the catch
-    const gridSize = 8;
-    const width = 4;
-    const studentCount = width * gridSize;
-    const preferenceCount = 2;
-    let students = [];
-    let seatingChartScore = 0;
-    class Student {
-      constructor(name_, frontPreference_, backPreference_, sitNextTo_, doNotSitNextTo_) {
-        this.name = name_;
-        this.frontPreference = frontPreference_;
-        this.backPreference = backPreference_;
-        this.sitNextTo = sitNextTo_;
-        this.doNotSitNextTo = doNotSitNextTo_;
-        this.happy = "";
-        this.sad = "";
-      }
-    }
     for (let i = 0; i < studentCount; i++) {
-      let sidePreference = Math.random() < 0.5;
-      students.push(new Student("Bob" + i, sidePreference, !sidePreference, new Array(preferenceCount), new Array(preferenceCount)));
+      let frontPref = Math.random();
+      let backPref = false;
+      if(frontPref < 0.75){
+        frontPref = true;
+      }else if(frontPref < 0.9){
+        frontPref = null;
+        backPref = null;
+      }else{
+        frontPref = false;
+        backPref = true;
+      }
+      let student = {
+          name: "Bob" + i,
+          frontPreference: frontPref,
+          backPreference: backPref,
+          sitNextTo: new Array(preferenceCount),
+          doNotSitNextTo: new Array(preferenceCount),
+          happy: "",
+          sad: "",
+      };
+      students.push(student)
+      // students.push(new Student("Bob" + i, sidePreference, !sidePreference, new Array(preferenceCount), new Array(preferenceCount)));
     }
     for (let i = 0; i < studentCount; i++) {
       let student = students[i];
-      let indexes = studentPreferenceIndexes(preferenceCount, i);
+      let indexes = studentPreferenceIndexes(preferenceCount, i, students);
       for (let j = 0; j < preferenceCount; j++) {
         student.sitNextTo[j] = students[indexes[j]].name;
         student.doNotSitNextTo[j] = students[indexes[j + preferenceCount]].name;
       }
     }
-    students = Object.values(hashMapStudents);
-    shuffleStudents();
-    countSeatingChartScore();
-    findOptimalSeatingChart();
-    // this is how you return item back to the frontend. if you have another list replace dummyList with your list
-    res.status(200).json({ studentList: students });
+    // students = Object.values(hashMapStudents);
+    shuffleStudents(students);
+    countSeatingChartScore(students);
+    const newStudentsAndScore = findOptimalSeatingChart(students);
+    const bestSeatingChartScore = newStudentsAndScore[0];
+    students = newStudentsAndScore[1];
+    console.log(newStudentsAndScore[1])
+    res.status(200).json({ studentList: students, bestSeatingChartScore});
   } catch (error) {
-    // print the error
     console.log(error);
     res.status(500).json({ error: 'Internal Error' });
   }
 }
-function studentPreferenceIndexes(preferenceCount_, selfIndex) {
+function studentPreferenceIndexes(preferenceCount_, selfIndex, students) {
   let indexes = [];
   let indexesLengh = 2 * preferenceCount_;
   let duplicates = false;
@@ -74,21 +80,21 @@ function studentPreferenceIndexes(preferenceCount_, selfIndex) {
   } while (duplicates);
   return indexes;
 }
-function countSeatingChartScore() {
+function countSeatingChartScore(students) {
   seatingChartScore = 0;
   for (let i = 0; i < students.length; i++) {
     let s = students[i];
     let left = i - 1;
     let right = i + 1;
-    let inFront = i - gridSize;
-    let behind = i + gridSize;
+    let inFront = i - gridWidth;
+    let behind = i + gridWidth;
     let closeIndexes = [left, right, inFront, behind, inFront - 1, inFront + 1, behind - 1, behind + 1];
     let closeNames = [];
     for (let j = 0; j < closeIndexes.length; j++) {
       let pos = closeIndexes[j];
       if (pos < 0 || pos >= students.length) continue;
-      if (pos == i + 1 && i + 1 % gridSize == 0) continue;
-      if (pos == i - 1 && i % gridSize == 0) continue;
+      if (pos == i + 1 && i + 1 % gridWidth == 0) continue;
+      if (pos == i - 1 && i % gridWidth == 0) continue;
       closeNames.push(students[pos].name);
     }
     // let closeNamesTemp = [];
@@ -102,43 +108,42 @@ function countSeatingChartScore() {
       let closeName = closeNames[j];
       if (s.sitNextTo.includes(closeName)) {
         seatingChartScore += 100;
-        s.happy += closeName + ", ";
+        s.happy += closeName + ", ";  
       } else if (s.doNotSitNextTo.includes(closeName)) {
         seatingChartScore -= 200;
         s.sad += closeName + ", ";
       }
     }
-    if (i < gridSize && s.frontPreference) seatingChartScore += 5;
-    if (students.length - i <= gridSize && s.backPreference) seatingChartScore += 5;
+    if (i < gridWidth && s.frontPreference == true) seatingChartScore += 30;
+    if (students.length - i <= gridWidth && s.backPreference == true) seatingChartScore += 30;
   }
 }
-function findOptimalSeatingChart() {
-  var t0 = performance.now()
-
+function findOptimalSeatingChart(students) {
+  // var t0 = performance.now()
+  let iterations = 1E5;
   let bestSeatingChart = students;
   let bestSeatingChartScore = 0;
-  for (let i = 0; i < 100000; i++) {
-    shuffleStudents();
-    countSeatingChartScore();
+  for (let i = 0; i < iterations; i++) {
+    shuffleStudents(students);
+    countSeatingChartScore(students);
     let currentSeatingChart = [];
     for (let j = 0; j < students.length; j++) currentSeatingChart.push(Object.assign({}, students[j]))
     if (seatingChartScore > bestSeatingChartScore) {
       bestSeatingChart = currentSeatingChart;
       bestSeatingChartScore = seatingChartScore;
+      console.log(bestSeatingChart[0].name + " " + bestSeatingChartScore)
     }
   }
-  students = bestSeatingChart;
-  console.log(bestSeatingChartScore)
-  printStudents(students)
-
-  var t1 = performance.now()
-  console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")
+  printStudents(bestSeatingChart)
+  return [bestSeatingChartScore, bestSeatingChart];
+  // var t1 = performance.now()
+  // console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")
 }
 function printStudents(students_) {
   let names = "", happy = "", frontPref = "", backPref = "", sitNextTo = "", doNotSitNextTo = "";
   for (let i = 0; i < students_.length; i++) {
     let student = students_[i];
-    if ((i + 1) % gridSize == 0) {
+    if ((i + 1) % gridWidth == 0) {
       names += student.name + " \n";
     } else {
       names += student.name + ", ";
@@ -157,7 +162,7 @@ function printStudents(students_) {
   // console.log("Sit Next To: " + sitNextTo);
   // console.log("Do Not Sit Next To: " + doNotSitNextTo);
 }
-function shuffleStudents() {
+function shuffleStudents(students) {
   var currentIndex = students.length, randomIndex;
   while (currentIndex != 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
