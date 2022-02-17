831328de708fe9598ea468a10da2bd8cfbc0cd54
Kefir101 <kefir.sade.bar@gmail.com>
2022-02-16 22:56:24 -0800

    assignSeats -> proper function names

--- server/routes/algorithm.js
+++ server/routes/algorithm.js
@@ -1,12 +1,11 @@
 const express = require("express");
 
 const { assignTableSeats } = require("../Controllers/tableSeatingAlg.js");
-// const { assignRandomSeats } = require("../Controllers/randomSeating.js");
+const { assignRandomSeats } = require("../Controllers/randomSeating.js");
 
 const router = express.Router();
 
-// rename the functions and import them here
-router.get("/randomize", assignSeats);
-router.get("/optimize", assignSeats);
+router.get("/randomize", assignRandomSeats);
+router.get("/optimize", assignTableSeats);
 
 module.exports = router;
